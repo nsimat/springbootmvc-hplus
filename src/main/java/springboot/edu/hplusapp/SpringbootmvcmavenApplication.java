f6caf5c659a32ec9ec3b3ab3af5edcd1a63e8db6
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2023-01-16 13:34:27 +0100

    Initial commit

---
+++ src/main/java/springboot/edu/hplusapp/SpringbootmvcmavenApplication.java
@@ -0,0 +1,13 @@
+package springboot.edu.hplusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcmavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmvcmavenApplication.class, args);
+    }
+
+}
